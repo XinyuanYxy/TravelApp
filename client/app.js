0a347abeeec68ed71084fde3bea29118d2461cd6
Yxychris <yxychris@outlook.com>
2021-06-14 15:24:08 -0400

    changed fetch to axios on the client side, made changes to the api key

--- client/app.js
+++ client/app.js
@@ -1,11 +1,16 @@
 // Personal API Key for OpenWeatherMap API
-const apiKey = '4a43c889a024d86915894a250382b482';
+const apiKey = '4a43c889a024d86915894a250382b482&units=metric';
+
 // Event listener to add function to existing HTML DOM element
 const generate = document.getElementById('generate');
 generate.addEventListener('click', async () => {
+	const zipcode = document.getElementById('zip').value;
 	try {
-		const zipcode = document.getElementById('zip').value;
-		const data = await getWebData(zipcode).then((data) => data.json());
+		if (isNaN(zipcode)) {
+			alert('Please enter a valid zipcode');
+			throw new Error('This is a not numeric value');
+		}
+		const data = await (await getWebData(zipcode)).data;
 		const date = new Date(data.dt * 1000 - data.timezone * 1000);
 		const temp = data.main.temp;
 		const content = document.getElementById('feelings').value;
@@ -20,7 +25,7 @@ generate.addEventListener('click', async () => {
 const getWebData = async (zipcode) => {
 	try {
 		//have to add http:// before the api link
-		const data = await fetch(
+		const data = await axios.get(
 			`http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${apiKey}`
 		);
 		return data;
@@ -46,10 +51,10 @@ const postData = async (url = '', dataObj = {}) => {
 /* Function to GET Project Data */
 const getData = async (url = '') => {
 	try {
-		const data = await fetch(url).then((data) => data.json());
-		const temp = data.temp;
-		const date = data.date;
-		const content = data.content;
+		const serverData = await axios.get('/all');
+		const temp = serverData.data.temp;
+		const date = serverData.data.date;
+		const content = serverData.data.content;
 
 		return {
 			date,
@@ -62,16 +67,13 @@ const getData = async (url = '') => {
 };
 
 const fillEntries = (obj = {}) => {
-	console.log('clicked');
 	let date = document.getElementById('date');
-	console.log(date);
 	let temp = document.getElementById('temp');
 	let content = document.getElementById('content');
 	date.innerHTML = `<lable>The Time is:</lable> 
                     <div>${obj.date} </div>`;
-	temp.innerHTML = `<lable>The temperature today is: </lable>
-                    <div>${obj.temp} </div>`;
+	temp.innerHTML = `<lable>The temperature today is:</lable>
+                    <div>${obj.temp} celsius </div>`;
 	content.innerHTML = `<lable>content: </lable>
                     <div>${obj.content} </div>`;
 };
-// module.exports = getWebData;
